680211e11e21ff92005b39015ffe7be8860def25
utkarsh45raj <utkarshraj1144@gmail.com>
2026-08-17 10:53:30 +0530

    Add files via upload

--- Main (1).java
+++ Main (1).java
@@ -6,10 +6,48 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-public class Main
-{
-	public static void main(String[] args) {
-		mother m = new child();
-		m.show();
-	}
+ 
+import java.util.*;
+
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter rows and cols for Matrix A: ");
+            int rA = sc.nextInt();
+            int cA = sc.nextInt();
+
+            int[][] A = new int[rA][cA];
+
+            for (int i = 0; i < rA; i++)
+                for (int j = 0; j < cA; j++)
+                    A[i][j] = sc.nextInt();
+
+            System.out.print("Enter rows and cols for Matrix B: ");
+            int rB = sc.nextInt();
+            int cB = sc.nextInt();
+
+            int[][] B = new int[rB][cB];
+
+            for (int i = 0; i < rB; i++)
+                for (int j = 0; j < cB; j++)
+                    B[i][j] = sc.nextInt();
+
+            int[][] result = multiplyMatrices(A, B);
+
+            System.out.println("Resulting Matrix:");
+
+            for (int[] row : result)
+                System.out.println(Arrays.toString(row));
+
+        } catch (MatrixMismatchException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
 }
